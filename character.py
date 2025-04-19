7017b24c9e2cfe1707fea08c00684f667b36c623
keerthana262007 <saivelavarthipati26@gmail.com>
2025-04-19 14:18:58 +0530

    Add files via upload

---
+++ character.py
@@ -0,0 +1,13 @@
+#take input from the user
+char=input("enter a single character:")
+if len(char)!=1:
+    print(f"please enter only one character!")
+else:
+    if char.isdigit():
+        print(f"'{char}'is a digit.")
+    elif char.islower():
+        print(f"'{char} is a lowercase  character.")
+    elif char.isupper():
+        print(f"'{char}'is an uppercase character.")
+    else:
+        print(f"'{char}' is a special character.")
